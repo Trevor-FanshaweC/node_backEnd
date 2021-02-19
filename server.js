bf37b995c9fc4e4fed27f983775ac173f1283d69
Trevor - FanshweC <halvas_trevor@MyOldMac.local>
2021-02-19 09:48:55 -0500

    adsffsddding latest and greatest

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 
 const port = process.env.PORT || 5050;
 
+app.use((req, res, next) => {
+    console.log('incoming request!');
+    debugger;
+    next();
+})
+
 app.use("/api", require('./routes/api'));
 
 app.listen(port, () => {
